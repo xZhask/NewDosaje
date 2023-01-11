e52f1aa0eb12d7acffe146b36bca8065e40d30e1
Josué <ajosue1994@gmail.com>
2023-01-11 14:41:41 -0500

    11Ene-EstDIRSA

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,19 +2,27 @@ const btnNuevoRegistro = document.querySelector('#btn-nuevo');
 const btnCambioPass = document.querySelector('#btnCambioPass');
 const modal = document.querySelector('#bg-modal');
 const modalContent = document.querySelector('#modal-content');
+const closeModal = document.querySelectorAll('.closeModal');
+
+const formCambioPass = document.querySelector('#frmCambioPass');
+const formNewRegistro = document.querySelector('#frmHojaRegistro');
 
 //Abrir Modal
-const abrirModal = (url) => {
+const abrirModal = (form) => {
     modal.style.display = 'table';
-    fetch(url).then(res => res.text()).then(res => modalContent.innerHTML = res);
+    form.style.display = 'flex'
 }
 //Cerrar Modal
 $('a.closeModal').on('click', () => { modal.style.display = 'none'; modalContent.classList.remove('frm-lg'); })
-
 //Botones para abrir modal
-btnNuevoRegistro.addEventListener('click', () => { modalContent.classList.add('frm-lg'); abrirModal('modals/frmRegistro.html') });
-btnCambioPass.addEventListener('click', () => abrirModal('modals/frmCambioPass.html'));
+btnNuevoRegistro.addEventListener('click', () => { modalContent.classList.add('frm-lg'); abrirModal(formNewRegistro) });
+btnCambioPass.addEventListener('click', () => abrirModal(formCambioPass));
 
-const btnMiau = document.querySelector('#BtnMiau');
-console.log(btnMiau);
-if (btnMiau) btnMiau.addEventListener('click', () => { alert('miau'); });
+formCambioPass.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('bien ctm')
+})
+formNewRegistro.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('bien ctm2')
+})
