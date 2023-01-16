61de01fc23ab6cc3b851533f670fc18579154d97
Josué <ajosue1994@gmail.com>
2023-01-16 16:32:00 -0500

    16ene23-dirsa

---
+++ App/controller/controllerPersona.php
@@ -0,0 +1,48 @@
+<?php
+//require_once '../models/ClsPersona.php';
+$accion = $_POST['accion'];
+controlador($accion);
+
+function controlador($accion)
+{
+    //$objPersona = new ClsPersona();
+
+    switch ($accion) {
+        case 'CONSULTA_DNI':
+            $dni = $_POST['dni'];
+            $token =
+                'e49fddfa2a41c2c2f26d48840f7d81a66dc78dc2b0e085742a883f0ab0f84158';
+            $url = "https://apiperu.dev/api/dni/$dni?api_token=$token";
+            $curl = curl_init();
+            $header = [];
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 1);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+            //para ejecutar los procesos de forma local en windows
+            //enlace de descarga del cacert.pem https://curl.haxx.se/docs/caextract.html
+            curl_setopt(
+                $curl,
+                CURLOPT_CAINFO,
+                dirname(__FILE__) . '/cacert-2023-01-10.pem'
+            );
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            if ($err) {
+                echo 'cURL Error #:' . $err;
+            } else {
+                echo $response;
+            }
+            break;
+            /* 
+        case 'BUSCAR_PERSONA':
+            $infoPersona = $objPersona->BuscarPersona($_POST['nrodoc']);
+            $infoPersona = $infoPersona->fetchAll(PDO::FETCH_OBJ);
+            echo json_encode($infoPersona);
+            break; */
+    }
+}
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,28 +1,100 @@
-const btnNuevoRegistro = document.querySelector('#btn-nuevo');
+/*----------------------- MODALS ------------------------------- */
 const btnCambioPass = document.querySelector('#btnCambioPass');
 const modal = document.querySelector('#bg-modal');
 const modalContent = document.querySelector('#modal-content');
-const closeModal = document.querySelectorAll('.closeModal');
-
-const formCambioPass = document.querySelector('#frmCambioPass');
-const formNewRegistro = document.querySelector('#frmHojaRegistro');
+const modalForm = document.querySelector('#modal_form')
 
 //Abrir Modal
 const abrirModal = (form) => {
     modal.style.display = 'table';
-    form.style.display = 'flex'
+    $('#modal_form').load(`App/views/modals/${form}`)
 }
 //Cerrar Modal
-$('a.closeModal').on('click', () => { modal.style.display = 'none'; modalContent.classList.remove('frm-lg'); })
-//Botones para abrir modal
-btnNuevoRegistro.addEventListener('click', () => { modalContent.classList.add('frm-lg'); abrirModal(formNewRegistro) });
-btnCambioPass.addEventListener('click', () => abrirModal(formCambioPass));
+$('a.closeModal').on('click', (e) => {
+    e.preventDefault();
+    modal.style.display = 'none';
+    modalContent.classList.remove('frm-lg');
+})
 
-formCambioPass.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log('bien ctm')
-})
-formNewRegistro.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log('bien ctm2')
-})
+//Botones para abrir modal
+btnCambioPass.addEventListener('click', () => abrirModal('frmCambioPass.html'));
+
+$(document).on("click", "#btn-nuevo", () => {
+    modalContent.classList.add('frm-lg')
+    abrirModal('frmNuevoRegistro.html')
+});
+
+/*----------------------- SECTIONS ------------------------------- */
+const lnkMuestra = document.querySelector('#lnk-muestra')
+const lnkReportes = document.querySelector('#lnk-reportes')
+const lnkUsuarios = document.querySelector('#lnk-usuarios')
+const contenedor = document.querySelector('#section_view');
+
+const loadView = async (e, lnk) => {
+    e.preventDefault()
+    $(".list_menu .active").removeClass('active');
+    lnk.classList.add('active');
+    $("#section_view").load(`App/views/sections/${lnk.pathname}`)
+};
+
+lnkMuestra.addEventListener('click', e => loadView(e, lnkMuestra))
+lnkReportes.addEventListener('click', e => loadView(e, lnkReportes))
+lnkUsuarios.addEventListener('click', e => loadView(e, lnkUsuarios))
+
+/*----------------------- FORMS ------------------------------- */
+async function postData(data, url) {
+    const response = await fetch(url, {
+        method: "POST",
+        body: data,
+    }).then((res) => res.json());
+    return await response;
+}
+async function buscarPersona(dni) {
+    let datos = new FormData();
+    datos.append("accion", "CONSULTA_DNI");
+    datos.append("dni", dni);
+    let persona = await (postData(datos, 'App/controller/controllerPersona.php'));
+    return persona
+}
+
+$(document).on("click", "#btn_search_user", async () => {
+    let dni = $('#nroDoc').val()
+    let persona = await buscarPersona(dni);
+    console.log(persona)
+});
+
+
+
+/* $(document).on("submit", "#frmCambioPass", (e) => {
+    e.preventDefault()
+    console.log('remiau')
+});
+ */
+/* 
+async function ListarPersonas(tipoPersona) {
+  let datos = new FormData();
+  datos.append("accion", "LISTAR_PERSONAS");
+  datos.append("tipoPersona", tipoPersona);
+  let personas = await (await postData(datos)).json();
+  tipoPersona == 'E' ? renderProfesionales(personas) : renderPacientes(personas);
+}
+
+async function postData(data) {
+    const response = await fetch("App/controller/controller.php", {
+        method: "POST",
+        body: data,
+    }).then((res) => res.json());
+    return await response;
+} 
+
+
+function abrir_seccion(lnk) {
+  $(".list-menu .active").removeClass("active");
+  $(`#${lnk.id}`).addClass("active");
+  fetch(`App/views/${lnk.attributes.href.nodeValue}`)
+    .then((res) => res.text())
+    .then((res) => $("#wrapper-section").html(res));
+}
+
+ <a id="lnk-horarios" href="/horarios.html">
+*/
